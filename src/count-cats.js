bab5ecc0ee75c76697be2c5b10e7a554fad2f9f9
xnapisatel <85700886+xnapisatel@users.noreply.github.com>
2022-10-14 14:20:56 +0300

    docs: complite

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats( matrix ) {
+  let result = 0;
+  for( i = 0; i< matrix.length; i++){
+    for ( j = 0; j< matrix[i].length; j++){
+      if( matrix[i][j]==='^^')
+      result++;
+    }
+  }
+  return result
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,20 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
- function deleteDigit(n) {
-  let num = String(n).split('');
-  let num_min = num[0];
- 
-  for(let i=0; i<num.length; i++) {
-   if (num[i] <= num_min) {
-     num_min != num[i];  
-     
-  }
-  
-  }
-  return Number(num.splice(num_min,2).join(''));
+function deleteDigit(n) {
+ let num = String(n).split('');
+ let num_min = num[0];
+
+ for(let i=0; i<num.length; i++) {
+  if (num[i] <= num_min) {
+    num_min != num[i];  
+    
+ }
  
  }
+ return Number(num.splice(num_min,1).join(''));
+
+}
 
 module.exports = {
   deleteDigit
